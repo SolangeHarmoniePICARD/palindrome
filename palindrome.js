f79a05f54241d8392638625b727a411b4db2ba03
SolangeHarmoniePICARD <solange.picard@outlook.com>
2019-07-06 11:13:53 +0200

    Développement de la fonction qui affichera le résultat du traitement

--- palindrome.js
+++ palindrome.js
@@ -1,11 +1,25 @@
-/* Projet de script en JS qui vérifie si le texte renseigné par l'utilisateur est un palindrome, c'est-à-dire un mot ou groupe de mots qui peut se lire indifféremment de gauche à droite ou de droite à gauche en gardant le même sens. */
+/* Script en JS qui vérifie si le texte renseigné par l'utilisateur est un palindrome, c'est-à-dire un mot ou groupe de mots qui peut se lire indifféremment de gauche à droite ou de droite à gauche en gardant le même sens. */
+
+var submit = document.getElementById("button");
 
 function palindrome(word) {
   word = word.toLowerCase().replace(/[^a-z0123456789]+/g,"");
   var reverse = word.split("").reverse().join("");
   if (word == reverse) {
-    return true
+    return true ;
   } else {
-    return false
+    return false ;
   }
 }
+
+submit.addEventListener("click", function() {
+    var text = document.getElementById("text").value;
+    var resultat = palindrome(text);
+    if(resultat === true) {
+        document.getElementById("notification").innerHTML = text + " est un palindrome";
+        document.getElementById("notification").className = "success";
+    } else {
+        document.getElementById("notification").innerHTML = text + " n'est pas un palindrome";
+        document.getElementById("notification").className = "danger";
+    }
+});
