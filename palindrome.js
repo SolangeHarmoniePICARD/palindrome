8dad5de0fd7e3bdc68ec8ac6e97b00534addd557
SolangeHarmoniePICARD <solange.picard@outlook.com>
2019-07-06 11:06:20 +0200

    Développement de la fonction palindrome qui prend comme seul argument 'word'

--- palindrome.js
+++ palindrome.js
@@ -1,1 +1,11 @@
 /* Projet de script en JS qui vérifie si le texte renseigné par l'utilisateur est un palindrome, c'est-à-dire un mot ou groupe de mots qui peut se lire indifféremment de gauche à droite ou de droite à gauche en gardant le même sens. */
+
+function palindrome(word) {
+  word = word.toLowerCase().replace(/[^a-z0123456789]+/g,"");
+  var reverse = word.split("").reverse().join("");
+  if (word == reverse) {
+    return true
+  } else {
+    return false
+  }
+}
